06baa5d7d606a1dd1c89fc8d27426a1860c6409c
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2018-09-08 19:52:04 -0400

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,2 +1,24 @@
 Import-Module model.psm1
 Import-Module view.psm1
+
+$looping = $true
+
+while ($looping){
+  mainMenu
+  
+  switch ($choice) {
+     "1" {
+        #Time Range Analysis
+        timeRangeMenu; break
+        }
+     "2" {
+        #Baseline of host
+        generalHuntSurvey; break
+        }
+     "3" {
+        #Locations/Commands of common processes
+        pathAndCommandLineOfProcesses; break
+        }
+
+  }
+}
