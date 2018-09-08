fcda1488f81dac3e276168bf7d5e32d5966cd44a
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2018-09-08 19:23:44 -0400

    Update view-mainmenu.ps1

--- View/view-mainmenu.ps1
+++ View/view-mainmenu.ps1
@@ -2,5 +2,6 @@ function mainMenu {
     Write-Host "CYBER RAMBO"
     Write-Host "1. Analyze Host Based on Time Range"
     Write-Host "2. Get a Baseline of Host"
+    Write-Host "3. View locations and commands of Common Processes"
     $choice = Read-Host " "
 }
