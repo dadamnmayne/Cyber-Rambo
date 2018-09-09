dfee13e409652a5aa5156edb79695ebdaa11b294
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2018-09-08 20:40:17 -0400

    Add files via upload

--- main.ps1
+++ main.ps1
@@ -1,24 +1,36 @@
-Import-Module model.psm1
-Import-Module view.psm1
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-$looping = $true
+Import-Module $scriptDir\model.psm1 -Force
+Import-Module $scriptDir\view.psm1 -Force
 
-while ($looping){
-  mainMenu
+clear
+mainMenu
+
   
-  switch ($choice) {
+ switch ($choice) {
+
      "1" {
+
         #Time Range Analysis
+        Write-Host "Test"
+        $looping = $False
         timeRangeMenu; break
+
         }
+
      "2" {
+
         #Baseline of host
         generalHuntSurvey; break
+
         }
+
      "3" {
+
         #Locations/Commands of common processes
         pathAndCommandLineOfProcesses; break
+
         }
 
   }
-}
+
--- view.psm1
+++ view.psm1
@@ -1,10 +1,18 @@
+
+
 function mainMenu {
+
+
     Write-Host "CYBER RAMBO"
     Write-Host "1. Analyze Host Based on Time Range"
     Write-Host "2. Get a Baseline of Host"
-    Write-Host "3. View locations and commands of Common Processes"
-    $choice = Read-Host " "
+    Write-Host "3. View locations and commands of Common Prxocesses"
+    $Global:choice = Read-Host " "
+
+
 }
+
+
 
 function timeRangeMenu {
     clear
@@ -13,22 +21,25 @@ function timeRangeMenu {
     while ($looping) {
     
         Write-Host "Enter Start time of range. For Example: 9:00AM 12/25/2000"
-        $start = Read-Host " "
+        $Global:start = Read-Host " "
 
         Write-Host "Enter End time of Range"
-        $end = Read-Host " "
+        $Global:end = Read-Host " "
 
-        Write-Host "Is this correct: Start Time:$start End Time:$End
+        Write-Host "Is this correct: Start Time:$start End Time:$End"
         Write-Host "Press Y or N"
 
         switch ($isCorrect){
             "Y" {
                 $looping = $false; break;
              }
+
              "N" {
                 break;
              }
         }
+
     }
+
     
 }
