6d7a9b4e155a90e801b5cf8e7612db29129124ae
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2018-09-10 12:16:15 -0400

    Update view.psm1

--- view.psm1
+++ view.psm1
@@ -1,31 +1,21 @@
-
-
 function mainMenu {
-
-
     Write-Host "CYBER RAMBO"
     Write-Host "1. Analyze Host Based on Time Range"
     Write-Host "2. Get a Baseline of Host"
     Write-Host "3. View locations and commands of Common Prxocesses"
     $Global:choice = Read-Host " "
-
-
 }
-
-
 
 function timeRangeMenu {
     clear
     $looping = $true
-    
     while ($looping) {
     
         Write-Host "Enter Start time of range. For Example: 9:00AM 12/25/2000"
         $Global:start = Read-Host " "
-
         Write-Host "Enter End time of Range"
+        
         $Global:end = Read-Host " "
-
         Write-Host "Is this correct: Start Time:$start End Time:$End"
         Write-Host "Press Y or N"
 
@@ -33,13 +23,10 @@ function timeRangeMenu {
             "Y" {
                 $looping = $false; break;
              }
-
              "N" {
                 break;
              }
         }
 
     }
-
-    
 }
